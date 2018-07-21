0f5b9e585862b3b7e0ab1c86757db9b629e53dff
mfekadu <mike.fekadu@gmail.com>
2018-07-20 22:02:35 -0700

    logical regression cost function + grad descent

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -39,14 +39,16 @@ grad = zeros(size(theta));
 
 
 
+h = sigmoid(X*theta);
 
-
-
+shift_theta = theta(2:size(theta));
+theta_reg = [0;shift_theta];
+J = (1/m)*(-y'* log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
 
 
 
 % =============================================================
 
-grad = grad(:);
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
 
 end
